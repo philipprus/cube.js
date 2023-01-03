f8107e454430b125d8582dc2df2514d3ccb9b159
Philipp Prus <philipp.prus@unityscm.com>
2023-01-03 09:45:36 +0200

    Added export dateParser

--- packages/cubejs-api-gateway/src/index.ts
+++ packages/cubejs-api-gateway/src/index.ts
@@ -3,4 +3,5 @@ export * from './sql-server';
 export * from './interfaces';
 export * from './CubejsHandlerError';
 export * from './UserError';
+export { dateParser } from './dateParser';
 export { getRequestIdFromRequest } from './requestParser';
